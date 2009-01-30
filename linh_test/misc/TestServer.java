f48e20da2c40ae720e8edd32586615d4e7555fc8
vtlinh87 <vtlinh87@e4c43e2e-de09-11dd-b58c-9b3bf6bc8b46>
2009-01-30 22:43:41 +0000

    Linh's branch test

---
+++ linh_test/misc/TestServer.java
@@ -0,0 +1,22 @@
+package misc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class TestServer {
+	
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		ServerSocket cs = new ServerSocket(2366);
+		  
+		
+		while(true) {
+			
+		}
+
+	}
+
+}
