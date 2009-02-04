acc0a8362e018d1147c1fe028b37d0b9c646e00c
pathikrit.bhowmick <pathikrit.bhowmick@e4c43e2e-de09-11dd-b58c-9b3bf6bc8b46>
2009-02-04 00:24:01 +0000

    Console connects to Server

--- src/client/Console.java
+++ src/client/Console.java
@@ -1,26 +1,83 @@
 package client;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Console {
 
-	private String server;
+	private static final String CONFIG_FILE = ".client";
 	
-	public Console(String server) {
-		this.server = server;
+	private String server;
+	private int port;
+	private Socket serverSocket;
+	
+	public Console() {
+		readConfig(CONFIG_FILE);
+		connectToServer();		
 	}
 	
+	public void execute() {
+		Scanner cmd = new Scanner(System.in);
+		for(String c; !(c = cmd.nextLine()).equalsIgnoreCase("exit"); ) {			
+			
+			try {
+				PrintWriter out = new PrintWriter(serverSocket.getOutputStream());
+				out.write(c+"\n");
+				out.flush();				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();			
+			}		
+		}
+		try {
+			serverSocket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void connectToServer() {		
+		try {
+			serverSocket = new Socket(server, port);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}	
+	}
+	
+	public void readConfig(String file) {
+		try {
+			for(Scanner sc = new Scanner(new File(file)); sc.hasNextLine(); ) {
+				String line = sc.nextLine().trim();
+				// TODO HashMap<String, String> config
+				if(line.startsWith("server"))
+					server = line.substring(line.indexOf("=")+1).trim();
+				else if(line.startsWith("port"))
+					port = Integer.parseInt(line.substring(line.indexOf("=")+1).trim());				
+			}
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 	public static void main(String[] args) {
-		
-//		Socket clientSocket = new Socket("127.0.0.1", 2366);
-//		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-//
-//		out.write("hello world");
-//		out.flush();
-//		
-//		clientSocket.close();		
+		// TODO args[0] can be used to overwrite defailt config file
+		Console c = new Console();
+		c.execute();
 	}
-
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,47 +1,102 @@
 package server;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 import java.util.Stack;
 
-import manager.Auction;
+//import manager.Auction;
 
 public class Server {
 
 	//private ports 49152–65535
-	public static final int PORT_BEGIN = 49152, PORT_END = 65535, PORT = 51065;
-	ServerSocket serverSocket;
-
-	private static Stack<Integer> freePorts = new Stack<Integer>();
+	public static final int PORT_BEGIN = 49152, PORT_END = 65535;
 	
-	static {
-		for(int i = PORT_BEGIN; i <= PORT_END; i++)
-			if(i != PORT)
-				freePorts.push(i);		
+	private ServerSocket serverSocket;
+	private int serverPort;
+	private static final String CONFIG_FILE = ".server";
+	private static Stack<Integer> freePorts = new Stack<Integer>();	
+	
+	public Server() {
+		readConfig(CONFIG_FILE);
+		try {
+			serverSocket = new ServerSocket(serverPort);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}		 		
+		init();		
 	}
 	
-	public Server() {		
+	public void init() {
+		for(int i = PORT_BEGIN; i <= PORT_END; i++)
+			if(i != serverPort)
+				freePorts.push(i);
+	}
+	
+	public void listen() {
+		
+        while(true) {
+        	Socket clientSocket;
+			try {
+				clientSocket = serverSocket.accept();
+				Scanner sc = new Scanner(clientSocket.getInputStream());
+				while(sc.hasNextLine()) {
+					String line = sc.nextLine();
+					System.out.println(line + " came from " + clientSocket.getInetAddress());					
+				}
+				clientSocket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }        
+        //TODO serverSocket.close();
+	}
+	
+	
+	public void readConfig(String file) {
 		try {
-			serverSocket = new ServerSocket(PORT);
-		} catch (IOException e) {
-			 System.err.println("Could not listen on port: " + PORT);
-		}		
+			for(Scanner sc = new Scanner(new File(file)); sc.hasNextLine(); ) {
+				String line = sc.nextLine().trim();				
+				if(line.startsWith("port"))
+					serverPort = Integer.parseInt(line.substring(line.indexOf("=")+1).trim());				
+			}
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void createAuction() {
-		for(boolean found = false; !found;) {
-			int port = freePorts.pop();
-			try {
-				ServerSocket auctionSocket = new ServerSocket(port);				
-				found = true;
-				Auction a = new Auction(auctionSocket);
-				
-			} catch (IOException e) {
-				 System.err.println("Could not listen on port: " + port);
-			}
-		}		
+//		for(boolean found = false; !found;) {
+//			int port = freePorts.pop();		
+//			ServerSocket auctionSocket;
+//			try {
+//				auctionSocket = new ServerSocket(port);
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}				
+//			found = true;
+//			//Auction a = new Auction(auctionSocket);
+//			
+//			
+//		}		
 	}
 	
-	
+	public static void main(String cmdLine[]) {
+		// TODO args[0] can be used to overwrite default config file
+		// TODO rename all dot files to .config 
+		Server s = new Server();
+		s.listen();
+	}
 	
 }
